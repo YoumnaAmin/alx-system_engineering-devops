c6aa5480619046368e1fafa933f24d9bc990d869
Youmna Amin <youmnamin564@gmail.com>
2024-06-20 00:56:23 +0300

    ..

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,27 +1,9 @@
-# Puppet manifest to fix Apache 500 Internal Server Error by correcting file permissions and ensuring Apache configuration
-
-exec { 'fix-apache-permissions':
-  command => '/bin/chown -R www-data:www-data /var/www/html && /bin/chmod -R 755 /var/www/html',
-  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
-  onlyif  => '/usr/bin/test $(stat -c %U /var/www/html) != www-data || /usr/bin/test $(stat -c %a /var/www/html) -ne 755',
-}
-
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => "
-<VirtualHost *:80>
-	ServerAdmin webmaster@localhost
-	DocumentRoot /var/www/html
-
-	ErrorLog \${APACHE_LOG_DIR}/error.log
-	CustomLog \${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>",
-  notify  => Exec['fix-apache-permissions'],
-}
-
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/apache2/sites-available/000-default.conf'],
-  require   => Exec['fix-apache-permissions'],
+# Web Stack debugging fix typo in config file
+exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
