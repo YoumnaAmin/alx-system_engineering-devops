e44e3acfb56edd4c5f510b27b33a486178135d48
Youmna Amin <youmnamin564@gmail.com>
2024-06-21 03:20:06 +0300

    ..

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,24 @@
 """subs module"""
 
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """to count numb of subscriber"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'my-app/0.0.1'}
-
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
+        js = response.json()
     except ValueError:
         return 0
+    data = js.get("data")
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
